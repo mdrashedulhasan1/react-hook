6623381cf5f50143ebdf37e4acc6457032b5707b
Rashedul Hasan <Rashedul Hasan>
2022-02-05 09:13:57 +0600

    React Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+  })
+const {userId, id, title} = users;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Total users: {users.length}</h1>
+        <h3>UserId: {userId}</h3>
+        <h2>Id: {id}</h2>
+        <h3>Title: {title}</h3>
     </div>
   );
 }
